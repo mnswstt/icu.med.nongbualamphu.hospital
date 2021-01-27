cc1d99bc9b1035b52ef7ede2855ecf89b9f5ed92
rn0z <manutsawee113@gmail.com>
2021-01-27 15:49:55 +0700

    try to fix bug js file didn't load

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     import _thread, time
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(server_address)
-    _thread.start_new_thread(lambda: io.run(app, debug=True), ())
+    _thread.start_new_thread(lambda: io.run(app), ())
 
     while True:
         data, address = s.recvfrom(4096)
