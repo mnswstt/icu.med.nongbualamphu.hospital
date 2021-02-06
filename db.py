77845ef69d202e95ce118ae036695ba139b2a462
rn0z <manutsawee113@gmail.com>
2021-02-06 16:25:44 +0700

    change db endpoint

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import pymongo
 
-client = pymongo.MongoClient("192.168.1.4", 27020)
+client = pymongo.MongoClient("127.0.0.1", 27020)
 db = client.icu_room
 icu_floor3 = db['icu-floor-3']
 #patient_list = db.patient_list
