27cefe01a299d50f492ad7b697d26f02b3029ff2
mnswstt <manutsawee113@gmail.com>
2021-02-07 21:51:17 +0700

    before deploy DONE!!

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from flask import Flask, jsonify, request, render_template
 from flask_socketio import SocketIO, emit
 from flask_bootstrap import Bootstrap
-import eventlet, socket, eventlet, functools, pymongo
-import services.manipulate_data as manipulate_data
-import pymongo
-
-client = pymongo.MongoClient("127.0.0.1", 27020)
-db = client.icu_room
-icu_floor3 = db['icu-floor-3']
+import eventlet, socket, eventlet, functools
+import db
 
 app = Flask(__name__)
 io = SocketIO(app, cors_allowed_origins="*")
@@ -34,18 +29,11 @@ def patient_list(raw, d):
 
 @app.route('/', methods=['GET'])
 def home():
+    db.icu_floor3.insert_one({'GOOD':'BOY'})
     return render_template('index.html')
 
 @app.route('/monitor')
 def rpi_monitor():
-    #d = []
-    #doct = ''
-    #for x in db.icu_floor3.find().sort('date', pymongo.DESCENDING).limit(1):
-        #db.icu_floor3.remove({'_id': x['_id']}) # ทดสอบๆ
-        #for i in range(len(x['caregiver_list'])):
-            #d.append((x['patient_list'][i], x['caregiver_list'][i]))
-        #doct = x['doct_shift']
-    #io.emit('patient_list', {'patient_list': d, 'doct_name': x['doct_shift']})
     return render_template('monitor.html', isOnline=internet_on())
 
 @app.route('/admin', methods=['GET', 'POST'])
@@ -53,7 +41,6 @@ def admin():
     if request.method == 'POST':
         raw = request.form.to_dict(flat=True)
         io.emit('patient_list', {'patient_list': patient_list(raw, [])})
-        icu_floor3.insert_one(manipulate_data.icu_f3(raw))
     return render_template('admin.html')
 
 @io.on('connected')
